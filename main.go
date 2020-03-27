236897f5e66a79039946e3506ac98ff24e4f82b9
swigley-tista <swigley@tistatech.com>
2020-03-27 13:49:41 -0500

    added swagger and tags back using swaggo

--- main.go
+++ main.go
@@ -1,11 +1,58 @@
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+// @query.collection.format multi
+
+// @securityDefinitions.basic BasicAuth
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @securitydefinitions.oauth2.application OAuth2Application
+// @tokenUrl https://example.com/oauth/token
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.implicit OAuth2Implicit
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.password OAuth2Password
+// @tokenUrl https://example.com/oauth/token
+// @scope.read Grants read access
+// @scope.write Grants write access
+// @scope.admin Grants read and write access to administrative information
+
+// @securitydefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl https://example.com/oauth/token
+// @authorizationurl https://example.com/oauth/authorize
+// @scope.admin Grants read and write access to administrative information
+
+// @x-extension-openapi {"example": "value on a json format"}
+
 package main
 
 import (
+	"avvio-api/docs"
+	_ "avvio-api/docs"
 	route_handlers "avvio-api/route-handlers"
 	"fmt"
-	"net/http"
-
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"net/http"
 )
 
 // The new router function creates the router and
@@ -17,16 +64,29 @@ func newRouter() *mux.Router {
 
 	// Declare the static file directory and point it to the directory we just made
 	staticFileDirectory := http.Dir("./assets/")
+	docFileDirectory := http.Dir("./docs")
+
 	// Declare the handler, that routes requests to their respective filename.
 	// The fileserver is wrapped in the `stripPrefix` method, because we want to
 	// remove the "/assets/" prefix when looking for files.
 	// For example, if we type "/assets/index.html" in our browser, the file server
 	// will look for only "index.html" inside the directory declared above.
 	// If we did not strip the prefix, the file server would look for "./assets/assets/index.html", and yield an error
+	docs.SwaggerInfo.Title = "AVVIO Swagger API"
+	docs.SwaggerInfo.Description = "This is the avvio API server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost"
+	docs.SwaggerInfo.BasePath = "/documentation"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
+
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
+	docFileHandler := http.StripPrefix("/documentation/doc.json", http.FileServer(docFileDirectory))
 	// The "PathPrefix" method acts as a matcher, and matches all routes starting
 	// with "/assets/", instead of the absolute route itself
+	r.PathPrefix("/docs").Handler(docFileHandler).Methods("GET")
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	httpSwagger.URL("/docs/doc.json")
+	r.PathPrefix("/documentation/").Handler(httpSwagger.WrapHandler)
 
 	r.HandleFunc("/task/{id}", route_handlers.GetTaskHandler).Methods("GET")
 	r.HandleFunc("/task", route_handlers.GetTaskHandler).Methods("GET")
@@ -35,6 +95,14 @@ func newRouter() *mux.Router {
 	return r
 }
 
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @termsOfService http://swagger.io/terms/
 func main() {
 	// The router is now formed by calling the `newRouter` constructor function
 	// that we defined above. The rest of the code stays the same
@@ -44,4 +112,8 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func docHandler(w http.ResponseWriter, r *http.Request) {
+
 }
