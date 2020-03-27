23269cdbe7bf2bd02aefe4323633b7ba8e1a654b
swigley-tista <swigley@tistatech.com>
2020-03-27 11:35:25 -0500

    removed swagger and it's autogenerated code

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
-// Testing go-swagger generation
-//
-// The purpose of this application is to test go-swagger in a simple GET request.
-//
-//     Schemes: http
-//     Host: localhost:8080
-//     Version: 0.0.1
-//     License: MIT http://opensource.org/licenses/MIT
-//     Contact: Daniel<danielfs.ti@gmail.com>
-//
-//     Consumes:
-//     - text/plain
-//
-//     Produces:
-//     - text/plain
-//
-// swagger:meta
 package main
 
 import (
-	"avvio-api/models"
-	"avvio-api/route-handlers"
+	route_handlers "avvio-api/route-handlers"
 	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/tidwall/buntdb"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // The new router function creates the router and
@@ -42,19 +23,15 @@ func newRouter() *mux.Router {
 	// For example, if we type "/assets/index.html" in our browser, the file server
 	// will look for only "index.html" inside the directory declared above.
 	// If we did not strip the prefix, the file server would look for "./assets/assets/index.html", and yield an error
-	r.HandleFunc("/swagger.json", swagger).Methods("GET")
 	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
 	// The "PathPrefix" method acts as a matcher, and matches all routes starting
 	// with "/assets/", instead of the absolute route itself
 	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
-	//r.HandleFunc("/team", route_handlers.GetTeamsHandler).Methods("GET")
-	r.HandleFunc("/team", route_handlers.CreateTeamHandler).Methods("POST")
-	r.HandleFunc("/team/{id}", route_handlers.GetTeamHandler).Methods("GET")
 
 	r.HandleFunc("/task/{id}", route_handlers.GetTaskHandler).Methods("GET")
 	r.HandleFunc("/task", route_handlers.GetTaskHandler).Methods("GET")
-	r.HandleFunc("/task", route_handlers.CreateTaskHandler).Methods("POST")
-	//r.HandleFunc("/application/{id}", )
+	r.HandleFunc("/task", route_handlers.CreateTaskHandler).Methods( "POST")
+
 	return r
 }
 
@@ -62,41 +39,9 @@ func main() {
 	// The router is now formed by calling the `newRouter` constructor function
 	// that we defined above. The rest of the code stays the same
 	r := newRouter()
-	db, err := buntdb.Open("data.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-
-	models.InitStore(db)
 	http.ListenAndServe(":8080", r)
 }
 
-// swagger:operation GET /hello/{name} hello Hello
-//
-// Returns a simple Hello message
-// ---
-// consumes:
-// - text/plain
-// produces:
-// - text/plain
-// parameters:
-// - name: name
-//   in: path
-//   description: Name to be returned.
-//   required: true
-//   type: string
-// responses:
-//   '200':
-//     description: The hello message
-//     type: string
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
-
-func swagger(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	http.ServeFile(w, r, "swagger.json")
-}
-
